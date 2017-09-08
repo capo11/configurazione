3f5a7b58a3c453513fb3fc60177584138d53fe3a
Andrea Capobianco <andreacapobianco29@gmail.com>
2017-09-08 15:45:25 +0100

    finito(?)

--- programma.py
+++ programma.py
@@ -3,7 +3,7 @@ from datetime import *
 import shutil
 import json
 import tarfile
-import ast
+import ftplib
 
 today = datetime.today()
 week = today.strftime("%U")
@@ -24,12 +24,15 @@ def loadConfig():
 jConf = loadConfig()
 master = jConf['file'][0]['Master']
 path36 = jConf['file'][0]['Cartella1']
-path37 = jConf['file'][0]['Cartella2']
+server = jConf['file'][0]['Server']
 usr1 = jConf['file'][0]['USR1']
 usr2 = jConf['file'][0]['USR2']
 # recupera il valore di un parametro dal json
 
 # funzione di ricerca nella cartella che si legge dal json
+
+ftp = ftplib.FTP('ftp.tabacchigest.com', 'gioi', 'test1234')
+ftp.cwd('./stage')
 
 
 def ricerca(pathCartella, usr):
@@ -40,10 +43,32 @@ def ricerca(pathCartella, usr):
             linea.strip()
             if week in linea:
                 shutil.move(pathCartella + '/' + linea, master)
+    else:
+        listadir = ftp.dir()
+        filematch = "*36*"
+        for filename in ftp.nlst(filematch):  # Loop - looking for matching files
+            fhandle = open(filename, 'wb')
+            print 'Getting ' + filename  #for confort sake, shows the file that's being retrieved
+            ftp.retrbinary('RETR ' + filename, fhandle.write)
+            fhandle.close()
+            shutil.move(filename, './server')
+
+            '''
+        for linea in listadir:
+            linea.strip()
+            if week in linea:
+                for filename in ftp.nlst(filematch):
+                    fhandle = open(os.path.join(pathCartella, filename), 'wb')
+                    ftp.retrbinary('RETR ' + filename, fhandle.write)
+                    # da sostituire con move to trash folder
+                    ftp.delete(filename)
+                    fhandle.close()
+                    except Exception as e:
+                '''
 
 
 ricerca(path36, usr1)
-ricerca(path37, usr2)
+ricerca(server, usr2)
 
 
 with tarfile.open('./Master/W' + week + '.tgz', 'w:gz') as tar:
