304f0c6e585fa5d4804b2135e087cd7b4797b548
Andrea Capobianco <andreacapobianco29@gmail.com>
2017-09-08 10:29:23 +0100

    config2

--- programma.py
+++ programma.py
@@ -1,10 +1,33 @@
 import os.path
 from datetime import *
 import shutil
+import json
+import tarfile
 
 today = datetime.today()
 week = today.strftime("%U")
-# config = open('nomefile', 'r')
+
+# carica file json
+
+
+def loadConfig():
+    try:
+        with open('./config.json') as confFile:
+            config = json.load(confFile)
+            confFile.close()
+            return config
+    except Exception as e:
+        print e
+
+
+jConf = loadConfig()
+master = jConf['file'][0]['Master']
+path36 = jConf['file'][0]['Cartella1']
+usr1 = jConf['file'][0]['USR']
+# recupera il valore di un parametro dal json
+print path36
+
+# funzione di ricerca nella cartella che si legge dal json
 
 
 def ricerca(pathCartella):
@@ -16,4 +39,8 @@ def ricerca(pathCartella):
             shutil.move(pathCartella + '/' + linea, './Master')
 
 
-ricerca('./W36')
+ricerca(path36)
+
+
+with tarfile.open('./Master/W' + week + '.tgz', 'w:gz') as tar:
+    tar.add('./W36/W' + week, arcname=os.path.basename('./W36/W' + week))
