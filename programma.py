c68051554d8d5498d715921ba52a05b2287ead97
Andrea Capobianco <andreacapobianco29@gmail.com>
2017-09-08 13:17:46 +0100

    prova

--- programma.py
+++ programma.py
@@ -3,6 +3,7 @@ from datetime import *
 import shutil
 import json
 import tarfile
+import ast
 
 today = datetime.today()
 week = today.strftime("%U")
@@ -24,26 +25,45 @@ jConf = loadConfig()
 master = jConf['file'][0]['Master']
 path36 = jConf['file'][0]['Cartella1']
 path37 = jConf['file'][0]['Cartella2']
-usr1 = jConf['file'][0]['USR']
+usr1 = jConf['file'][0]['USR1']
+usr2 = jConf['file'][0]['USR2']
 # recupera il valore di un parametro dal json
-print path36
 
 # funzione di ricerca nella cartella che si legge dal json
 
 
-def ricerca(pathCartella):
-    listadir = os.listdir(pathCartella)
+def ricerca(pathCartella, usr):
+    if(usr == ""):
+        listadir = os.listdir(pathCartella)
 # il path del listdir deve essere conosciuto precedentemente
+        for linea in listadir:
+            linea.strip()
+            if week in linea:
+                shutil.move(pathCartella + '/' + linea, master)
+
+
+ricerca(path36, usr1)
+ricerca(path37, usr2)
+
+
+with tarfile.open('./Master/W' + week + '.tgz', 'w:gz') as tar:
+    listadir = os.listdir('./Master')
     for linea in listadir:
         linea.strip()
-        if week in linea:
-            shutil.move(pathCartella + '/' + linea, master)
+        tar.add('./Master/' + linea, arcname=os.path.basename('./Master/' + linea))
 
-
-ricerca(path36)
-ricerca(path37)
-
-'''
-with tarfile.open('./Master/W' + week + '.tgz', 'w:gz') as tar:
-    tar.add('./W36/W' + week, arcname=os.path.basename('./W36/W' + week))
-'''
+listadir = os.listdir('./Master')
+linea = week
+contaneg = 0
+contapos = 0
+for linea in listadir:
+    linea.strip()
+    primo = open('./Master/' + linea)
+    for riga in primo.readlines():
+        if((int(float(riga))) < 0):
+            contaneg = contaneg + int(float(riga))
+        else:
+            contapos = contapos + int(float(riga))
+log = open('analisi.log', 'w')
+log.writelines('Settimana ' + week + '  ')
+log.write('positivi: ' + str(contapos) + ', negativi: ' + str(contaneg))
